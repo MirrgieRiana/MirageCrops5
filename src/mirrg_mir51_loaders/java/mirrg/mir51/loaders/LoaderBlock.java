ebc7f712d8bfc5179f1e70f01df0a974f47099f4
MirrgieRiana <devnull@localhost>
2015-01-29 17:47:31 +0900

    lledo il /LoaderRecipe LoaderBlock/

---
+++ src/mirrg_mir51_loaders/java/mirrg/mir51/loaders/LoaderBlock.java
@@ -0,0 +1,49 @@
+package mirrg.mir51.loaders;
+
+import java.util.function.Supplier;
+
+import mirrg.mir50.loader.EnumLoadEventTiming;
+import mirrg.mir50.loader.Loader;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class LoaderBlock extends Loader<Block>
+{
+
+	protected Supplier<Block> supplierBlock;
+	protected Class<? extends ItemBlock> itemclass;
+	protected String name;
+	protected LoaderCreativeTab loaderCreativeTab;
+
+	public void init(Supplier<Block> supplierBlock, Class<? extends ItemBlock> itemclass, String name)
+	{
+		this.supplierBlock = supplierBlock;
+		this.itemclass = itemclass;
+		this.name = name;
+	}
+
+	public void setCreativeTab(LoaderCreativeTab loaderCreativeTab)
+	{
+		this.loaderCreativeTab = loaderCreativeTab;
+	}
+
+	@Override
+	protected void loadDependancies(EnumLoadEventTiming loadEvent)
+	{
+		super.loadDependancies(loadEvent);
+		if (loaderCreativeTab != null) loaderCreativeTab.load(loadEvent);
+	}
+
+	@Override
+	protected void loadThisLoader(EnumLoadEventTiming loadEvent)
+	{
+		if (loadEvent == EnumLoadEventTiming.PreInit) {
+			Block block = supplierBlock.get();
+			if (loaderCreativeTab != null) block.setCreativeTab(loaderCreativeTab.get());
+			GameRegistry.registerBlock(block, itemclass, name);
+			set(block);
+		}
+	}
+
+}
---
+++ src/mirrg_mir51_loaders/java/mirrg/mir51/loaders/LoaderRecipe.java
@@ -0,0 +1,25 @@
+package mirrg.mir51.loaders;
+
+import mirrg.mir50.loader.EnumLoadEventTiming;
+import mirrg.mir50.loader.Loader;
+
+public class LoaderRecipe extends Loader<Void>
+{
+
+	protected Runnable runner;
+
+	public void init(Runnable runner)
+	{
+		this.runner = runner;
+	}
+
+	@Override
+	protected void loadThisLoader(EnumLoadEventTiming loadEvent)
+	{
+		if (loadEvent == EnumLoadEventTiming.PostInit) {
+			runner.run();
+			loadCompleted();
+		}
+	}
+
+}
