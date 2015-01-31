9f63d5a090570a36cfc734bbacd0415fc940c278
MirrgieRiana <devnull@localhost>
2015-02-01 07:19:50 +0900

    lledo il Hammer ekee pia ri trrodo il IronIngot ik IronOre ijjud kretroima ee ik ModuleMirageCropsCore

--- src/main/java/com/example/examplemod/ModuleMirageCropsCore.java
+++ src/main/java/com/example/examplemod/ModuleMirageCropsCore.java
@@ -1,18 +1,29 @@
 package com.example.examplemod;
 
 import mirrg.mir50.block.render.multiple.BlockMultipleRendering;
+import mirrg.mir50.item.AdaptorItemContainerItemCraftingTool;
+import mirrg.mir50.item.ItemMir50;
 import mirrg.mir50.modding.ModuleAbstract;
 import mirrg.mir51.loaders.LoaderBlock;
 import mirrg.mir51.loaders.LoaderCreativeTab;
+import mirrg.mir51.loaders.LoaderItem;
+import mirrg.mir51.loaders.LoaderRecipe;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModuleMirageCropsCore extends ModuleAbstract
 {
 
 	public LoaderCreativeTab loaderCreativeTab = new LoaderCreativeTab();
 	public LoaderBlock loaderBlock = new LoaderBlock();
+	public LoaderItem loaderItem = new LoaderItem();
 
 	public ModuleMirageCropsCore()
 	{
@@ -30,6 +41,35 @@ public class ModuleMirageCropsCore extends ModuleAbstract
 		}, ItemBlock.class, "oreCalciteGroup");
 		loaderBlock.setCreativeTab(loaderCreativeTab);
 		add(loaderBlock);
+
+		loaderItem.init(() -> {
+			ItemMir50 itemMir50 = new ItemMir50();
+
+			itemMir50.setMaxStackSize(1);
+			itemMir50.setTextureName("minecraft:iron_pickaxe");
+
+			itemMir50.setMaxDamage(20 - 1);
+			itemMir50.adaptorItemContainerItem = new AdaptorItemContainerItemCraftingTool(itemMir50);
+
+			return itemMir50;
+		}, "craftingToolHammerIron", ModMirageCrops.MODID);
+		loaderItem.setCreativeTab(loaderCreativeTab);
+		add(loaderItem);
+
+		add(new LoaderRecipe(() -> {
+
+			GameRegistry.addRecipe(new ShapedOreRecipe(
+				new ItemStack(Items.iron_ingot, 1, 0),
+				"X",
+				"Y",
+				'X', new ItemStack(loaderItem.get(), 1, OreDictionary.WILDCARD_VALUE),
+				'Y', Blocks.iron_ore));
+
+		}) {
+			{
+				dependsOn(loaderItem);
+			}
+		});
 
 	}
 
