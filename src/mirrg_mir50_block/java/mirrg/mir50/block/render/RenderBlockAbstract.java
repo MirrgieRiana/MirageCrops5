06afa3deae6b0a0a51f084ed5ac502c2f8297044
MirrgieRiana <devnull@localhost>
2015-01-31 14:45:18 +0900

    lledo il renderCubeInInventory in RenderBlockAbstract

--- src/mirrg_mir50_block/java/mirrg/mir50/block/render/RenderBlockAbstract.java
+++ src/mirrg_mir50_block/java/mirrg/mir50/block/render/RenderBlockAbstract.java
@@ -2,6 +2,7 @@ package mirrg.mir50.block.render;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.relauncher.Side;
@@ -36,5 +37,52 @@ public abstract class RenderBlockAbstract implements ISimpleBlockRenderingHandle
 	@Override
 	public abstract boolean renderWorldBlock(IBlockAccess blockAccess, int x, int y, int z,
 		Block block, int modelId, RenderBlocks renderer);
+
+	public static void renderCubeInInventory(Block block, int metadata, RenderBlocks renderer)
+	{
+		renderCubeInInventory(block, metadata, renderer, 0xffffff);
+	}
+
+	public static void renderCubeInInventory(Block block, int metadata, RenderBlocks renderer, int color)
+	{
+		Tessellator t = Tessellator.instance;
+
+		t.startDrawingQuads();
+		t.setNormal(0.0F, -1.0F, 0.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 0, metadata));
+		t.draw();
+
+		t.startDrawingQuads();
+		t.setNormal(0.0F, 1.0F, 0.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 1, metadata));
+		t.draw();
+
+		t.startDrawingQuads();
+		t.setNormal(0.0F, 0.0F, -1.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 2, metadata));
+		t.draw();
+
+		t.startDrawingQuads();
+		t.setNormal(0.0F, 0.0F, 1.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 3, metadata));
+		t.draw();
+
+		t.startDrawingQuads();
+		t.setNormal(-1.0F, 0.0F, 0.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 4, metadata));
+		t.draw();
+
+		t.startDrawingQuads();
+		t.setNormal(1.0F, 0.0F, 0.0F);
+		Tessellator.instance.setColorOpaque_I(color);
+		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
+		t.draw();
+
+	}
 
 }
