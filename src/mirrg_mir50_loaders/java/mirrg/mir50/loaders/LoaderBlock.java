5362dc8e4b6f17a7674a612a629f5c8a28ac6dbe
MirrgieRiana <devnull@localhost>
2015-02-03 17:47:51 +0900

    LoaderBlock ri trrodo il /itemBlockIniter itemConstructorArguments/

--- src/mirrg_mir50_loaders/java/mirrg/mir50/loaders/LoaderBlock.java
+++ src/mirrg_mir50_loaders/java/mirrg/mir50/loaders/LoaderBlock.java
@@ -1,10 +1,12 @@
 package mirrg.mir50.loaders;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import mirrg.mir50.loader.EnumLoadEventTiming;
 import mirrg.mir50.loader.Loader;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -14,13 +16,21 @@ public class LoaderBlock extends Loader<Block>
 	protected Supplier<Block> supplierBlock;
 	protected Class<? extends ItemBlock> itemclass;
 	protected String name;
+	protected Object[] itemConstructorArguments;
 	protected LoaderCreativeTab loaderCreativeTab;
+	protected Consumer<Item> itemBlockIniter;
 
-	public void init(Supplier<Block> supplierBlock, Class<? extends ItemBlock> itemclass, String name)
+	public void init(Supplier<Block> supplierBlock, Class<? extends ItemBlock> itemclass, String name, Object... itemConstructorArguments)
 	{
 		this.supplierBlock = supplierBlock;
 		this.itemclass = itemclass;
 		this.name = name;
+		this.itemConstructorArguments = itemConstructorArguments;
+	}
+
+	public void setItemBlockIniter(Consumer<Item> itemBlockIniter)
+	{
+		this.itemBlockIniter = itemBlockIniter;
 	}
 
 	public void setCreativeTab(LoaderCreativeTab loaderCreativeTab)
@@ -41,7 +51,8 @@ public class LoaderBlock extends Loader<Block>
 		if (loadEvent == EnumLoadEventTiming.PreInit) {
 			Block block = supplierBlock.get();
 			if (loaderCreativeTab != null) block.setCreativeTab(loaderCreativeTab.get());
-			GameRegistry.registerBlock(block, itemclass, name);
+			GameRegistry.registerBlock(block, itemclass, name, itemConstructorArguments);
+			if (itemBlockIniter != null) itemBlockIniter.accept(Item.getItemFromBlock(block));
 			set(block);
 		}
 	}
