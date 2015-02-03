142ccfb3679c547151e7077a489ac6ad188c7bbb
MirrgieRiana <devnull@localhost>
2015-02-03 17:52:23 +0900

    ModuleMirageCropsCore ri trrodo il "blockCalciteGroup" ekee pia ri jjudo il AdaptorBlockMultipleRenderingMulti

--- src/mirrg_miragecrops5/java/mirrg_miragecrops5/ModuleMirageCropsCore.java
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/ModuleMirageCropsCore.java
@@ -1,5 +1,8 @@
 package mirrg_miragecrops5;
 
+import mirrg.mir50.block.multi.AdaptorBlockHarvestMulti;
+import mirrg.mir50.block.multi.AdaptorBlockSubBlocksMulti;
+import mirrg.mir50.block.multi.ItemBlockMulti;
 import mirrg.mir50.item.AdaptorItemContainerItemCraftingTool;
 import mirrg.mir50.item.ItemMir50;
 import mirrg.mir50.loaders.LoaderBlock;
@@ -9,7 +12,11 @@ import mirrg.mir50.loaders.LoaderOreGen;
 import mirrg.mir50.loaders.LoaderRecipe;
 import mirrg.mir50.modding.ModuleAbstract;
 import mirrg.mir50.worldgen.ore.GeneratorOreXZWrapperWorldGenerator;
+import mirrg.mir51.render.block.multiple.AdaptorBlockMultipleRendering;
 import mirrg.mir51.render.block.multiple.BlockMultipleRendering;
+import mirrg.mir51.render.block.multiple.multi.AdaptorBlockMultipleRenderingMulti;
+import mirrg.mir51.render.block.multiple.multi.ContainerMetaBlockMultipleRendering;
+import mirrg.mir51.render.block.multiple.multi.MetaBlockMultipleRendering;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -26,7 +33,10 @@ public class ModuleMirageCropsCore extends ModuleAbstract
 
 	public LoaderCreativeTab loaderCreativeTab = new LoaderCreativeTab();
 	public LoaderBlock loaderBlock = new LoaderBlock();
+	public LoaderBlock loaderBlock2 = new LoaderBlock();
 	public LoaderItem loaderItem = new LoaderItem();
+
+	public ContainerMetaBlockMultipleRendering container1;
 
 	public ModuleMirageCropsCore()
 	{
@@ -44,6 +54,64 @@ public class ModuleMirageCropsCore extends ModuleAbstract
 		}, ItemBlock.class, "oreCalciteGroup");
 		loaderBlock.setCreativeTab(loaderCreativeTab);
 		add(loaderBlock);
+
+		loaderBlock2.init(() -> {
+			BlockMultipleRendering block = new BlockMultipleRendering(Material.rock);
+
+			{
+				ContainerMetaBlockMultipleRendering container = new ContainerMetaBlockMultipleRendering(16);
+
+				block.setAdaptorBlockMultipleRendering(new AdaptorBlockMultipleRenderingMulti(block, container));
+				block.adaptorBlockHarvest = new AdaptorBlockHarvestMulti(block, container);
+				block.adaptorBlockSubBlocks = new AdaptorBlockSubBlocksMulti(block, container);
+
+				{
+					int metaId = 0;
+
+					MetaBlockMultipleRendering metaBlockMultipleRendering = new MetaBlockMultipleRendering(block, metaId);
+
+					{
+						AdaptorBlockMultipleRendering adaptorBlockMultipleRendering = new AdaptorBlockMultipleRendering(block);
+
+						adaptorBlockMultipleRendering.appendIcon("minecraft:stone");
+						adaptorBlockMultipleRendering.appendIcon("miragecrops5:oreCalcite", color(255, 205, 59));
+
+						metaBlockMultipleRendering.setAdaptorBlockMultipleRendering(adaptorBlockMultipleRendering);
+					}
+
+					container.set(metaId, metaBlockMultipleRendering);
+				}
+
+				{
+					int metaId = 1;
+
+					MetaBlockMultipleRendering metaBlockMultipleRendering = new MetaBlockMultipleRendering(block, metaId);
+
+					{
+						AdaptorBlockMultipleRendering adaptorBlockMultipleRendering = new AdaptorBlockMultipleRendering(block);
+
+						adaptorBlockMultipleRendering.appendIcon("minecraft:stone");
+						adaptorBlockMultipleRendering.appendIcon("miragecrops5:oreOtavite");
+
+						metaBlockMultipleRendering.setAdaptorBlockMultipleRendering(adaptorBlockMultipleRendering);
+					}
+
+					container.set(metaId, metaBlockMultipleRendering);
+				}
+
+				container1 = container;
+			}
+
+			block.adaptorBlockMultipleRendering.appendIcon("minecraft:stone");
+			block.adaptorBlockMultipleRendering.appendIcon("miragecrops5:oreCalcite", color(255, 205, 59));
+
+			return block;
+		}, ItemBlockMulti.class, "blockCalciteGroup");
+		loaderBlock2.setItemBlockIniter(item -> {
+			((ItemBlockMulti) item).init(container1);
+		});
+		loaderBlock2.setCreativeTab(loaderCreativeTab);
+		add(loaderBlock2);
 
 		loaderItem.init(() -> {
 			ItemMir50 itemMir50 = new ItemMir50();
