05fe81b41e201bf09a95783ba32a8287c1b361c9
MirrgieRiana <devnull@localhost>
2015-02-09 04:49:06 +0900

    lledo il HelpersOreDictionary

---
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/HelpersOreDictionary.java
@@ -0,0 +1,40 @@
+package mirrg_miragecrops5;
+
+import java.util.ArrayList;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class HelpersOreDictionary
+{
+
+	public static ItemStack getOrThrow(String name)
+	{
+		ArrayList<ItemStack> ores = OreDictionary.getOres(name);
+		if (ores.size() == 0) throw new NullPointerException("ore not found: " + name);
+		return ores.get(0);
+	}
+
+	public static ItemStack copyOrThrow(String name)
+	{
+		return getOrThrow(name).copy();
+	}
+
+	public static ArrayList<String> getNames(ItemStack itemStack)
+	{
+		ArrayList<String> names = new ArrayList<>();
+
+		for (String name : OreDictionary.getOreNames()) {
+			ore:
+			for (ItemStack ore : OreDictionary.getOres(name)) {
+				if (OreDictionary.itemMatches(ore, itemStack, false)) {
+					names.add(name);
+					break ore;
+				}
+			}
+		}
+
+		return names;
+	}
+
+}
