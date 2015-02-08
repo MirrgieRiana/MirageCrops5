f8fb44784997164e3866382aaa81a5383f4d9f44
MirrgieRiana <devnull@localhost>
2015-02-08 23:31:49 +0900

    lledo il EnumNBTType

---
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/EnumNBTType.java
@@ -0,0 +1,17 @@
+package mirrg_miragecrops5;
+
+public enum EnumNBTType
+{
+	END,
+	BYTE, // primitive
+	SHORT, // primitive
+	INT, // primitive
+	LONG, // primitive
+	FLOAT, // primitive
+	DOUBLE, // primitive
+	BYTE_ARRAY, // array
+	STRING, // object
+	LIST,
+	COMPOUND, // object
+	INT_ARRAY, // array
+}
