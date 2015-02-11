eaa8ad5034de3e6ec3109421e6eef9a731b1a99f
MirrgieRiana <devnull@localhost>
2015-02-11 15:26:13 +0900

    lledo il LoaderGuiHandler

---
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/LoaderGuiHandler.java
@@ -0,0 +1,25 @@
+package mirrg_miragecrops5;
+
+import java.util.function.Supplier;
+
+import mirrg.mir50.guihandler.GuiHandler;
+import mirrg.mir50.loader.EnumLoadEventTiming;
+import mirrg.mir50.loader.Loader;
+import cpw.mods.fml.common.network.NetworkRegistry;
+
+public class LoaderGuiHandler extends Loader<GuiHandler>
+{
+
+	public Supplier<Object> supplierMod = null;
+	public int guiId = 0;
+
+	@Override
+	protected void loadThisLoader(EnumLoadEventTiming loadEvent)
+	{
+		if (loadEvent == EnumLoadEventTiming.Init) {
+			set(new GuiHandler(guiId));
+			NetworkRegistry.INSTANCE.registerGuiHandler(supplierMod.get(), get());
+		}
+	}
+
+}
