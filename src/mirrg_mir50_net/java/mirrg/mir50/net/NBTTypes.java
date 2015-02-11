cf8a4dd4d2ba4d2ef1d627c161cd03ac82db6af8
MirrgieRiana <devnull@localhost>
2015-02-11 14:21:24 +0900

    mirrg_miragecrops5/EnumNBTType -> mirrg/mir50/net/NBTTypes

---
+++ src/mirrg_mir50_net/java/mirrg/mir50/net/NBTTypes.java
@@ -0,0 +1,18 @@
+package mirrg.mir50.net;
+
+public class NBTTypes
+{
+	public static final int
+		END = 0,
+		BYTE = 1, // primitive
+		SHORT = 2, // primitive
+		INT = 3, // primitive
+		LONG = 4, // primitive
+		FLOAT = 5, // primitive
+		DOUBLE = 6, // primitive
+		BYTE_ARRAY = 7, // array
+		STRING = 8, // object
+		LIST = 9,
+		COMPOUND = 10, // object
+		INT_ARRAY = 11; // array
+}
