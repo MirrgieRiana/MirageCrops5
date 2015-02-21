c197f807d1975115952a3adff38de06134d40aa2
MirrgieRiana <devnull@localhost>
2015-02-21 10:09:43 +0900

    lledo il TileEntityMMFEasy. TileEntityMMF/Furnace Macerator/ ri hyshoreloeto

--- src/mirrg_miragecrops5/java/mirrg_miragecrops5/machine/TileEntityMMFFurnace.java
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/machine/TileEntityMMFFurnace.java
@@ -4,74 +4,54 @@ import java.util.function.IntConsumer;
 
 import mirrg.mir50.datamodels.DatamodelEnergy;
 import mirrg.mir50.gui.containerextraslots.ContainerExtraSlotDatamodel;
-import mirrg.mir50.gui.containerextraslots.ContainerExtraSlotLabel;
-import mirrg.mir50.world.pointer.SupplierPositionWorldFromTileEntity;
-import mirrg.mir51.gui.renderers.RendererEnergySlotMeter;
 import mirrg.mir51.gui.renderers.RendererEnergySlotProgress;
 import mirrg.mir51.gui.renderers.RendererEnergySlotProgress.EnumProgressAlign;
-import mirrg.mir51.gui.renderers.RendererLabel;
 import mirrg.mir51.inventory.IInventoryMir51;
 import mirrg.mir51.inventory.InventoryMir51Base;
-import mirrg.mir51.inventory.InventoryMir51Chain;
-import mirrg.mir51.inventory.InventoryMir51FromInventory;
-import mirrg.mir51.inventory.InventoryMir51Trimmer;
 import mirrg.mir51.modding.HelpersSide;
 import mirrg.mir52.gui.ContainerMir52;
 import mirrg.mir52.gui.SupplierPositionContainerFlow;
 import mirrg_miragecrops5.fairytype.HelpersFairyType;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntityFurnace;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.ForgeDirection;
-import api.mirrg.mir50.gui.renderer.EnumTextAlign;
 import api.mirrg.mir50.gui.renderer.IRenderer;
 
-public class TileEntityMMFFurnace extends TileEntityMMF
+public class TileEntityMMFFurnace extends TileEntityMMFEasy
 {
 
 	public final IInventoryMir51 inventoryInMaterial;
 	public final IInventoryMir51 inventoryInMaterialProcessing;
 	public final IInventoryMir51 inventoryInFuel;
-	public final IInventoryMir51 inventoryOut;
-	public final IInventoryMir51 inventoryOutProcessing;
-	public final IInventoryMir51 inventoryFairy;
-	public final IInventoryMir51 inventoryFairyFuel;
 
-	public final DatamodelEnergy energyTankProcessing;
 	public final DatamodelEnergy energyTankFuel;
-	public final DatamodelEnergy energyTankHyleon;
-
-	public final DatamodelFairyValues fairyValues;
-
-	protected int ticker = 0;
 
 	public TileEntityMMFFurnace()
 	{
 		inventoryInMaterial = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 2), "inventoryInMaterial");
 		inventoryInMaterialProcessing = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 1), "inventoryInMaterialProcessing");
 		inventoryInFuel = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 3), "inventoryInFuel");
-		inventoryOut = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 4), "inventoryOut");
-		inventoryOutProcessing = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 4), "inventoryOutProcessing");
-		inventoryFairy = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 3), "inventoryFairy");
-		inventoryFairyFuel = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 1), "inventoryFairyFuel");
 
 		inventoryChain.add(inventoryInMaterial);
 		inventoryChain.add(inventoryInMaterialProcessing);
 		inventoryChain.add(inventoryInFuel);
-		inventoryChain.add(inventoryOut);
-		inventoryChain.add(inventoryOutProcessing);
-		inventoryChain.add(inventoryFairy);
-		inventoryChain.add(inventoryFairyFuel);
 
-		energyTankProcessing = add(new DatamodelEnergy(this::markDirty, 0), "energyTankProcessing");
 		energyTankFuel = add(new DatamodelEnergy(this::markDirty, 0), "energyTankFuel");
-		energyTankHyleon = add(new DatamodelEnergy(this::markDirty, 100000), "energyTankHyleon");
+	}
 
-		fairyValues = new DatamodelFairyValues(new int[] {
+	@Override
+	public String getDefaultName()
+	{
+		return "container.miragecrops5.mmfFurnace";
+	}
+
+	@Override
+	protected int[] getFairyValues()
+	{
+		return new int[] {
 			0, 0, 0, -7, 0, 0,
-		}, inventoryFairy);
+		};
 	}
 
 	@Override
@@ -81,14 +61,6 @@ public class TileEntityMMFFurnace extends TileEntityMMF
 			inventoryOut,
 			inventoryInFuel,
 			inventoryInMaterial,
-		};
-	}
-
-	@Override
-	protected IInventoryMir51[] getInventoryExtract(int side, ItemStack itemStack)
-	{
-		return new IInventoryMir51[] {
-			inventoryOut,
 		};
 	}
 
@@ -106,53 +78,13 @@ public class TileEntityMMFFurnace extends TileEntityMMF
 		}
 	}
 
-	@Override
-	protected ResourceLocation getGuiTexture(ContainerMir52 container)
-	{//machineMirageFairy
-		return new ResourceLocation("miragecrops5" + ":" + "textures/gui/NULL_GUI_TEXTURE.png");
-	}
-
-	@Override
-	protected boolean hasGui()
-	{
-		return true;
-	}
-
 	public final static IRenderer<ContainerExtraSlotDatamodel<DatamodelEnergy>> rendererFuel =
 		new RendererEnergySlotProgress("fuel", EnumProgressAlign.DOWN);
 
 	@Override
 	protected void prepareContainerSlots(ContainerMir52 container)
 	{
-		{
-			ContainerExtraSlotLabel containerExtraSlot;
-
-			containerExtraSlot = new ContainerExtraSlotLabel(getLocalizedName(),
-				gui -> gui.getGuiWidth() / 2,
-				gui -> TOP,
-				0x404040, EnumTextAlign.CENTER);
-			containerExtraSlot.renderer = new RendererLabel(containerExtraSlot);
-			container.addContainerExtraSlot(containerExtraSlot, "labelTileEntity");
-
-			containerExtraSlot = new ContainerExtraSlotLabel(I18n.format("container.inventory"),
-				gui -> SHIFT,
-				gui -> gui.getGuiHeight() - 96 + 2,
-				0x404040, EnumTextAlign.LEFT);
-			containerExtraSlot.renderer = new RendererLabel(containerExtraSlot);
-			container.addContainerExtraSlot(containerExtraSlot, "labelInventory");
-		}
-		{
-			ContainerExtraSlotFairyGraph containerExtraSlot = new ContainerExtraSlotFairyGraph(
-				LEFT + 9 * 2, TOP_CHEST, 9 * 2, 9 * 6, fairyValues);
-			containerExtraSlot.renderer = new RendererFairyGraph();
-			container.addContainerExtraSlot(containerExtraSlot, "labelFairyTypes");
-		}
-
-		InventoryMir51Chain inventoryChest = inventoryChain;
-		IInventoryMir51 inventory2 = new InventoryMir51FromInventory(container.getPlayer().inventory,
-			new SupplierPositionWorldFromTileEntity(this));
-		InventoryMir51Trimmer inventoryPlayer = new InventoryMir51Trimmer(inventory2, 9, 27);
-		InventoryMir51Trimmer inventoryHandle = new InventoryMir51Trimmer(inventory2, 0, 9);
+		super.prepareContainerSlots(container);
 
 		container.addInventory(inventoryInMaterial,
 			new SupplierPositionContainerFlow(LEFT + 9 * 4, TOP_CHEST, SHIFT, SHIFT, 9), true);
@@ -161,35 +93,15 @@ public class TileEntityMMFFurnace extends TileEntityMMF
 			new SupplierPositionContainerFlow(LEFT + 9 * 8, TOP_CHEST, SHIFT, SHIFT, 9), false);
 		container.addInventory(inventoryInFuel,
 			new SupplierPositionContainerFlow(LEFT + 9 * 4, TOP_CHEST + 9 * 4, SHIFT, SHIFT, 9), true);
-		container.addInventory(inventoryOut,
-			new SupplierPositionContainerFlow(LEFT + 9 * 14, TOP_CHEST + 9 * 1, SHIFT, SHIFT, 2), false);
-		container.addInventory(inventoryFairy,
-			(inventory, index, x, y) -> new SlotFairy(inventory, index, x, y),
-			new SupplierPositionContainerFlow(LEFT, TOP_CHEST, SHIFT, SHIFT, 1), false);
-		container.addInventory(inventoryFairyFuel,
-			(inventory, index, x, y) -> new SlotFairyFuel(inventory, index, x, y),
-			new SupplierPositionContainerFlow(LEFT + 9 * 11, TOP_CHEST + 9 * 4, SHIFT, SHIFT, 1), false);
 
-		container.addInventory(inventoryPlayer,
-			new SupplierPositionContainerFlow(LEFT, TOP_INVENTORY, SHIFT, SHIFT, 9), true);
-		container.addInventory(inventoryHandle,
-			new SupplierPositionContainerFlow(LEFT, TOP_HOLDING, SHIFT, SHIFT, 9), true);
+		container.setTransferInventories(inventoryInMaterial, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryInFuel, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryOut, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryFairy, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryFairyFuel, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryPlayer.get(), inventoryFairy, inventoryFairyFuel, inventoryInMaterial, inventoryInFuel);
+		container.setTransferInventories(inventoryHandle.get(), inventoryFairy, inventoryFairyFuel, inventoryInMaterial, inventoryInFuel);
 
-		container.setTransferInventories(inventoryInMaterial, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryInFuel, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryOut, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryFairy, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryFairyFuel, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryPlayer, inventoryFairy, inventoryFairyFuel, inventoryInMaterial, inventoryInFuel);
-		container.setTransferInventories(inventoryHandle, inventoryFairy, inventoryFairyFuel, inventoryInMaterial, inventoryInFuel);
-
-		{
-			ContainerExtraSlotDatamodel<DatamodelEnergy> containerExtraSlot =
-				new ContainerExtraSlotDatamodel<DatamodelEnergy>(
-					LEFT + 9 * 12 - 24 / 2, TOP_CHEST + 9 * 2 - 17 / 2 - 3, 24, 17, energyTankProcessing);
-			containerExtraSlot.renderer = RendererEnergySlotProgress.instanceLeft;
-			container.addContainerExtraSlot(containerExtraSlot, getName(energyTankProcessing));
-		}
 		{
 			ContainerExtraSlotDatamodel<DatamodelEnergy> containerExtraSlot =
 				new ContainerExtraSlotDatamodel<DatamodelEnergy>(
@@ -197,14 +109,6 @@ public class TileEntityMMFFurnace extends TileEntityMMF
 			containerExtraSlot.renderer = rendererFuel;
 			container.addContainerExtraSlot(containerExtraSlot, getName(energyTankFuel));
 		}
-		{
-			ContainerExtraSlotDatamodel<DatamodelEnergy> containerExtraSlot =
-				new ContainerExtraSlotDatamodel<DatamodelEnergy>(
-					LEFT + 9 * 12 - 0 / 2, TOP_CHEST + 9 * 4 - 0 / 2 - 3, 0, 0, energyTankHyleon);
-			containerExtraSlot.renderer = RendererEnergySlotMeter.instance;
-			container.addContainerExtraSlot(containerExtraSlot, getName(energyTankHyleon));
-		}
-
 	}
 
 	@Override
@@ -279,12 +183,6 @@ public class TileEntityMMFFurnace extends TileEntityMMF
 
 			energyTankFuel.amount -= cooldown;
 		}
-	}
-
-	@Override
-	public String getDefaultName()
-	{
-		return "container.miragecrops5.mmfFurnace";
 	}
 
 }
--- src/mirrg_miragecrops5/java/mirrg_miragecrops5/machine/TileEntityMMFMacerator.java
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/machine/TileEntityMMFMacerator.java
@@ -9,65 +9,41 @@ import java.util.Map.Entry;
 import java.util.function.IntConsumer;
 
 import mirrg.mir50.datamodels.DatamodelEnergy;
-import mirrg.mir50.gui.containerextraslots.ContainerExtraSlotDatamodel;
-import mirrg.mir50.gui.containerextraslots.ContainerExtraSlotLabel;
-import mirrg.mir50.world.pointer.SupplierPositionWorldFromTileEntity;
-import mirrg.mir51.gui.renderers.RendererEnergySlotMeter;
-import mirrg.mir51.gui.renderers.RendererEnergySlotProgress;
-import mirrg.mir51.gui.renderers.RendererLabel;
 import mirrg.mir51.inventory.IInventoryMir51;
 import mirrg.mir51.inventory.InventoryMir51Base;
-import mirrg.mir51.inventory.InventoryMir51Chain;
-import mirrg.mir51.inventory.InventoryMir51FromInventory;
-import mirrg.mir51.inventory.InventoryMir51Trimmer;
 import mirrg.mir51.modding.HelpersSide;
 import mirrg.mir52.gui.ContainerMir52;
 import mirrg.mir52.gui.SupplierPositionContainerFlow;
 import mirrg_miragecrops5.fairytype.HelpersFairyType;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import api.mirrg.mir50.gui.renderer.EnumTextAlign;
 
-public class TileEntityMMFMacerator extends TileEntityMMF
+public class TileEntityMMFMacerator extends TileEntityMMFEasy
 {
 
 	public final IInventoryMir51 inventoryInMaterial;
 	public final IInventoryMir51 inventoryInMaterialProcessing;
-	public final IInventoryMir51 inventoryOut;
-	public final IInventoryMir51 inventoryOutProcessing;
-	public final IInventoryMir51 inventoryFairy;
-	public final IInventoryMir51 inventoryFairyFuel;
-
-	public final DatamodelEnergy energyTankProcessing;
-	public final DatamodelEnergy energyTankHyleon;
-
-	public final DatamodelFairyValues fairyValues;
-
-	protected int ticker = 0;
 
 	public TileEntityMMFMacerator()
 	{
 		inventoryInMaterial = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 2), "inventoryInMaterial");
 		inventoryInMaterialProcessing = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 1), "inventoryInMaterialProcessing");
-		inventoryOut = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 4), "inventoryOut");
-		inventoryOutProcessing = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 4), "inventoryOutProcessing");
-		inventoryFairy = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 3), "inventoryFairy");
-		inventoryFairyFuel = add(new InventoryMir51Base(this::markDirty, getSupplierPosition(), 1), "inventoryFairyFuel");
 
 		inventoryChain.add(inventoryInMaterial);
 		inventoryChain.add(inventoryInMaterialProcessing);
-		inventoryChain.add(inventoryOut);
-		inventoryChain.add(inventoryOutProcessing);
-		inventoryChain.add(inventoryFairy);
-		inventoryChain.add(inventoryFairyFuel);
+	}
 
-		energyTankProcessing = add(new DatamodelEnergy(this::markDirty, 0), "energyTankProcessing");
-		energyTankHyleon = add(new DatamodelEnergy(this::markDirty, 100000), "energyTankHyleon");
+	@Override
+	public String getDefaultName()
+	{
+		return "container.miragecrops5.mmfMacerator";
+	}
 
-		fairyValues = new DatamodelFairyValues(new int[] {
+	@Override
+	protected int[] getFairyValues()
+	{
+		return new int[] {
 			0, -48, 0, 0, 0, 0,
-		}, inventoryFairy);
+		};
 	}
 
 	@Override
@@ -80,14 +56,6 @@ public class TileEntityMMFMacerator extends TileEntityMMF
 	}
 
 	@Override
-	protected IInventoryMir51[] getInventoryExtract(int side, ItemStack itemStack)
-	{
-		return new IInventoryMir51[] {
-			inventoryOut,
-		};
-	}
-
-	@Override
 	protected IInventoryMir51[] getInventoryInsert(int side, ItemStack itemStack)
 	{
 		return new IInventoryMir51[] {
@@ -96,91 +64,22 @@ public class TileEntityMMFMacerator extends TileEntityMMF
 	}
 
 	@Override
-	protected ResourceLocation getGuiTexture(ContainerMir52 container)
-	{//machineMirageFairy
-		return new ResourceLocation("miragecrops5" + ":" + "textures/gui/NULL_GUI_TEXTURE.png");
-	}
-
-	@Override
-	protected boolean hasGui()
-	{
-		return true;
-	}
-
-	@Override
 	protected void prepareContainerSlots(ContainerMir52 container)
 	{
-		{
-			ContainerExtraSlotLabel containerExtraSlot;
-
-			containerExtraSlot = new ContainerExtraSlotLabel(getLocalizedName(),
-				gui -> gui.getGuiWidth() / 2,
-				gui -> TOP,
-				0x404040, EnumTextAlign.CENTER);
-			containerExtraSlot.renderer = new RendererLabel(containerExtraSlot);
-			container.addContainerExtraSlot(containerExtraSlot, "labelTileEntity");
-
-			containerExtraSlot = new ContainerExtraSlotLabel(I18n.format("container.inventory"),
-				gui -> SHIFT,
-				gui -> gui.getGuiHeight() - 96 + 2,
-				0x404040, EnumTextAlign.LEFT);
-			containerExtraSlot.renderer = new RendererLabel(containerExtraSlot);
-			container.addContainerExtraSlot(containerExtraSlot, "labelInventory");
-		}
-		{
-			ContainerExtraSlotFairyGraph containerExtraSlot = new ContainerExtraSlotFairyGraph(
-				LEFT + 9 * 2, TOP_CHEST, 9 * 2, 9 * 6, fairyValues);
-			containerExtraSlot.renderer = new RendererFairyGraph();
-			container.addContainerExtraSlot(containerExtraSlot, "labelFairyTypes");
-		}
-
-		InventoryMir51Chain inventoryChest = inventoryChain;
-		IInventoryMir51 inventory2 = new InventoryMir51FromInventory(container.getPlayer().inventory,
-			new SupplierPositionWorldFromTileEntity(this));
-		InventoryMir51Trimmer inventoryPlayer = new InventoryMir51Trimmer(inventory2, 9, 27);
-		InventoryMir51Trimmer inventoryHandle = new InventoryMir51Trimmer(inventory2, 0, 9);
+		super.prepareContainerSlots(container);
 
 		container.addInventory(inventoryInMaterial,
 			new SupplierPositionContainerFlow(LEFT + 9 * 4, TOP_CHEST + 9 * 2, SHIFT, SHIFT, 9), true);
 		container.addInventory(inventoryInMaterialProcessing,
 			(inventory, index, x, y) -> new SlotProcessing(inventory, index, x, y),
 			new SupplierPositionContainerFlow(LEFT + 9 * 8, TOP_CHEST + 9 * 2, SHIFT, SHIFT, 9), false);
-		container.addInventory(inventoryOut,
-			new SupplierPositionContainerFlow(LEFT + 9 * 14, TOP_CHEST + 9 * 1, SHIFT, SHIFT, 2), false);
-		container.addInventory(inventoryFairy,
-			(inventory, index, x, y) -> new SlotFairy(inventory, index, x, y),
-			new SupplierPositionContainerFlow(LEFT, TOP_CHEST, SHIFT, SHIFT, 1), false);
-		container.addInventory(inventoryFairyFuel,
-			(inventory, index, x, y) -> new SlotFairyFuel(inventory, index, x, y),
-			new SupplierPositionContainerFlow(LEFT + 9 * 11, TOP_CHEST + 9 * 4, SHIFT, SHIFT, 1), false);
 
-		container.addInventory(inventoryPlayer,
-			new SupplierPositionContainerFlow(LEFT, TOP_INVENTORY, SHIFT, SHIFT, 9), true);
-		container.addInventory(inventoryHandle,
-			new SupplierPositionContainerFlow(LEFT, TOP_HOLDING, SHIFT, SHIFT, 9), true);
-
-		container.setTransferInventories(inventoryInMaterial, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryOut, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryFairy, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryFairyFuel, inventoryHandle, inventoryPlayer);
-		container.setTransferInventories(inventoryPlayer, inventoryFairy, inventoryFairyFuel, inventoryInMaterial);
-		container.setTransferInventories(inventoryHandle, inventoryFairy, inventoryFairyFuel, inventoryInMaterial);
-
-		{
-			ContainerExtraSlotDatamodel<DatamodelEnergy> containerExtraSlot =
-				new ContainerExtraSlotDatamodel<DatamodelEnergy>(
-					LEFT + 9 * 12 - 24 / 2, TOP_CHEST + 9 * 2 - 17 / 2 - 3, 24, 17, energyTankProcessing);
-			containerExtraSlot.renderer = RendererEnergySlotProgress.instanceLeft;
-			container.addContainerExtraSlot(containerExtraSlot, getName(energyTankProcessing));
-		}
-		{
-			ContainerExtraSlotDatamodel<DatamodelEnergy> containerExtraSlot =
-				new ContainerExtraSlotDatamodel<DatamodelEnergy>(
-					LEFT + 9 * 12 - 0 / 2, TOP_CHEST + 9 * 4 - 0 / 2 - 3, 0, 0, energyTankHyleon);
-			containerExtraSlot.renderer = RendererEnergySlotMeter.instance;
-			container.addContainerExtraSlot(containerExtraSlot, getName(energyTankHyleon));
-		}
-
+		container.setTransferInventories(inventoryInMaterial, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryOut, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryFairy, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryFairyFuel, inventoryHandle.get(), inventoryPlayer.get());
+		container.setTransferInventories(inventoryPlayer.get(), inventoryFairy, inventoryFairyFuel, inventoryInMaterial);
+		container.setTransferInventories(inventoryHandle.get(), inventoryFairy, inventoryFairyFuel, inventoryInMaterial);
 	}
 
 	@Override
@@ -265,12 +164,6 @@ public class TileEntityMMFMacerator extends TileEntityMMF
 
 		}
 
-	}
-
-	@Override
-	public String getDefaultName()
-	{
-		return "container.miragecrops5.mmfMacerator";
 	}
 
 }
