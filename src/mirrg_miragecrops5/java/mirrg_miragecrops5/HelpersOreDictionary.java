a8e72c39c3d247aaedad33b821929c55807536b0
MirrgieRiana <devnull@localhost>
2015-02-26 20:30:39 +0900

    lledo il /get copy getOrThrow copyOrThrow/ in HelpersOreDictionary

--- src/mirrg_miragecrops5/java/mirrg_miragecrops5/HelpersOreDictionary.java
+++ src/mirrg_miragecrops5/java/mirrg_miragecrops5/HelpersOreDictionary.java
@@ -8,6 +8,19 @@ import net.minecraftforge.oredict.OreDictionary;
 public class HelpersOreDictionary
 {
 
+	public static ItemStack get(String name)
+	{
+		ArrayList<ItemStack> ores = OreDictionary.getOres(name);
+		if (ores.size() == 0) return null;
+		return ores.get(0);
+	}
+
+	public static ItemStack copy(String name)
+	{
+		ItemStack itemStack = get(name);
+		return itemStack != null ? itemStack.copy() : null;
+	}
+
 	public static ItemStack getOrThrow(String name)
 	{
 		ArrayList<ItemStack> ores = OreDictionary.getOres(name);
@@ -18,6 +31,36 @@ public class HelpersOreDictionary
 	public static ItemStack copyOrThrow(String name)
 	{
 		return getOrThrow(name).copy();
+	}
+
+	public static ItemStack get(String name, int stackSize)
+	{
+		ItemStack itemStack = get(name);
+		if (itemStack == null) return null;
+		itemStack.stackSize = stackSize;
+		return itemStack;
+	}
+
+	public static ItemStack copy(String name, int stackSize)
+	{
+		ItemStack itemStack = copy(name);
+		if (itemStack == null) return null;
+		itemStack.stackSize = stackSize;
+		return itemStack;
+	}
+
+	public static ItemStack getOrThrow(String name, int stackSize)
+	{
+		ItemStack itemStack = getOrThrow(name);
+		itemStack.stackSize = stackSize;
+		return itemStack;
+	}
+
+	public static ItemStack copyOrThrow(String name, int stackSize)
+	{
+		ItemStack itemStack = copyOrThrow(name);
+		itemStack.stackSize = stackSize;
+		return itemStack;
 	}
 
 	public static ArrayList<String> getNames(ItemStack itemStack)
