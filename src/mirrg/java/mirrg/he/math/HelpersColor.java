1d8dcd571584e879e2ded835343aa634235785ff
MirrgieRiana <devnull@localhost>
2015-02-26 21:31:24 +0900

    lledo il /ib3 id3 multiplicate/ in HelpersColor

--- src/mirrg/java/mirrg/he/math/HelpersColor.java
+++ src/mirrg/java/mirrg/he/math/HelpersColor.java
@@ -3,9 +3,30 @@ package mirrg.he.math;
 public class HelpersColor
 {
 
-	public static int color(int r, int g, int b)
+	@Deprecated
+	public static int color(int r, int g, int b) // TODO
 	{
-		return (r << 16) | (g << 8) | b;
+		return ib3(r, g, b);
+	}
+
+	public static int ib3(int r, int g, int b)
+	{
+		return (HelpersMath.trim(r, 0, 255) << 16)
+			| (HelpersMath.trim(g, 0, 255) << 8)
+			| HelpersMath.trim(b, 0, 255);
+	}
+
+	public static int id3(double r, double g, double b)
+	{
+		return ib3((int) (r * 255), (int) (g * 255), (int) (b * 255));
+	}
+
+	public static int multiplicate(int color, double rate)
+	{
+		int r = (color >> 16) & 0xFF;
+		int g = (color >> 8) & 0xFF;
+		int b = color & 0xFF;
+		return ib3((int) Math.round(r * rate), (int) Math.round(g * rate), (int) Math.round(b * rate));
 	}
 
 }
