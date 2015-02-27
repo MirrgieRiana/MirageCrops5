c8c5caa4d703c3793a41c14396a544694b09f2e0
MirrgieRiana <devnull@localhost>
2015-02-27 09:07:52 +0900

    qero il kommenta im BlockMir50

--- src/mirrg_mir50_block/java/mirrg/mir50/block/BlockMir50.java
+++ src/mirrg_mir50_block/java/mirrg/mir50/block/BlockMir50.java
@@ -58,49 +58,6 @@ public class BlockMir50 extends Block implements IVirtualClass
 
 	/////////////////////////////////////////////////////////////////////
 
-	//TODO
-	/*
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int getBlockColor()
-	{
-		double d0 = 0.5D;
-		double d1 = 1.0D;
-		return ColorizerGrass.getGrassColor(d0, d1);
-	}
-
-		@Override
-		@SideOnly(Side.CLIENT)
-		public int getRenderColor(int p_149741_1_)
-		{
-			return this.getBlockColor();
-		}
-
-		@Override
-		@SideOnly(Side.CLIENT)
-		public int colorMultiplier(IBlockAccess p_149720_1_, int p_149720_2_, int p_149720_3_, int p_149720_4_)
-		{
-			int l = 0;
-			int i1 = 0;
-			int j1 = 0;
-
-			for (int k1 = -1; k1 <= 1; ++k1)
-			{
-				for (int l1 = -1; l1 <= 1; ++l1)
-				{
-					int i2 = p_149720_1_.getBiomeGenForCoords(p_149720_2_ + l1, p_149720_4_ + k1).getBiomeGrassColor(p_149720_2_ + l1, p_149720_3_, p_149720_4_ + k1);
-					l += (i2 & 16711680) >> 16;
-					i1 += (i2 & 65280) >> 8;
-					j1 += i2 & 255;
-				}
-			}
-
-			return 0xffffff - ((l / 9 & 255) << 16 | (i1 / 9 & 255) << 8 | j1 / 9 & 255);
-		}
-	*/
-
-	/////////////////////////////////////////////////////////////////////
-
 	public final IVirtualImplementationAccessor<IAdaptorBlockSubBlocks> accessor_IAdaptorBlockSubBlocks;
 
 	@Override
@@ -283,42 +240,6 @@ public class BlockMir50 extends Block implements IVirtualClass
 	{
 		return accessor_IAdaptorBlockHarvest.get().getExpDrop(world, meta, fortune);
 	}
-
-	//TODO
-	/*
-	@SideOnly(Side.CLIENT)
-	public int getBlockColor()
-	{
-		return 16777215;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int getRenderColor(int p_149741_1_)
-	{
-		return 16777215;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int colorMultiplier(IBlockAccess p_149720_1_, int p_149720_2_, int p_149720_3_, int p_149720_4_)
-	{
-		return 16777215;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int getRenderBlockPass()
-	{
-		return 0;
-	}
-
-	@Override
-	public boolean canRenderInPass(int pass)
-	{
-		return pass == getRenderBlockPass();
-	}
-	*/
 
 	/////////////////////////////////////////////////////////////////////
 
