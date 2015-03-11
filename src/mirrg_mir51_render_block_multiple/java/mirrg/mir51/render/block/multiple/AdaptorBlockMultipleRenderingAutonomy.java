763f5b13732dada6fcfdf944c2041c70e3cfe5f1
MirrgieRiana <devnull@localhost>
2015-03-11 09:43:00 +0900

    moizo il AdaptorBlockMultipleRenderingAutonomy

--- src/mirrg_mir51_render_block_multiple/java/mirrg/mir51/render/block/multiple/AdaptorBlockMultipleRenderingAutonomy.java
+++ src/mirrg_mir51_render_block_multiple/java/mirrg/mir51/render/block/multiple/AdaptorBlockMultipleRenderingAutonomy.java
@@ -103,6 +103,14 @@ public class AdaptorBlockMultipleRenderingAutonomy extends AdaptorBlockIconOverr
 	@SideOnly(Side.CLIENT)
 	public boolean shouldSideBeRendered(IBlockAccess blockAccess, int x, int y, int z, int side)
 	{
+		if (side == 0 && owner.getField_minY() > 0.0D) return true;
+		if (side == 1 && owner.getField_maxY() < 1.0D) return true;
+		if (side == 2 && owner.getField_minZ() > 0.0D) return true;
+		if (side == 3 && owner.getField_maxZ() < 1.0D) return true;
+		if (side == 4 && owner.getField_minX() > 0.0D) return true;
+		if (side == 5 && owner.getField_maxX() < 1.0D) return true;
+		if (blockAccess.getBlock(x, y, z).isOpaqueCube()) return false;
+
 		if (!HelpersBlockMultipleRendering.enabled) return true;
 		return HelpersBlockMultipleRendering.visibles[side];
 	}
